346c68af735db8e8881d3fbbceb4902f18218b8f
rgervacio0803 <ogturtel@yahoo.com>
2025-09-04 16:06:39 -0700

    Added animation on some features

--- src/components/home/BrowseByCategory.jsx
+++ src/components/home/BrowseByCategory.jsx
@@ -13,37 +13,37 @@ const BrowseByCategory = () => {
             </div>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
-            <Link to="/explore" className="icon-box style-2 rounded">
+            <Link to="/explore" className="icon-box style-2 rounded" data-aos="fade-left" data-aos-delay="0">
               <i className="fa fa-image"></i>
               <span>Art</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
-            <Link to="/explore" className="icon-box style-2 rounded">
+            <Link to="/explore" className="icon-box style-2 rounded" data-aos="fade-left" data-aos-delay="100">
               <i className="fa fa-music"></i>
               <span>Music</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
-            <Link to="/explore" className="icon-box style-2 rounded">
+            <Link to="/explore" className="icon-box style-2 rounded" data-aos="fade-left" data-aos-delay="200">
               <i className="fa fa-search"></i>
               <span>Domain Names</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
-            <Link to="/explore" className="icon-box style-2 rounded">
+            <Link to="/explore" className="icon-box style-2 rounded" data-aos="fade-left" data-aos-delay="300">
               <i className="fa fa-globe"></i>
               <span>Virtual Worlds</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
-            <Link to="/explore" className="icon-box style-2 rounded">
+            <Link to="/explore" className="icon-box style-2 rounded" data-aos="fade-left" data-aos-delay="400">
               <i className="fa fa-vcard"></i>
               <span>Trading Cards</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
-            <Link to="/explore" className="icon-box style-2 rounded">
+            <Link to="/explore" className="icon-box style-2 rounded" data-aos="fade-left" data-aos-delay="500">
               <i className="fa fa-th"></i>
               <span>Collectibles</span>
             </Link>
